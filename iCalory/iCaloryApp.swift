9a19ed7bf22058db5d068b914f53b64417a9b179
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-04-10 16:41:37 +0500

    Initial Commit

---
+++ iCalory/iCaloryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloryApp.swift
+//  iCalory
+//
+//  Created by Павел Кай on 10.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct iCaloryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
